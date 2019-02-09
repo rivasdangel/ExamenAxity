5981744879d8990beed9e4988e50e43424acad8d
Leonardo Rivas <leonardorivasd@gmail.com>
2019-02-09 01:01:36 -0600

    Se crean instancia de los TextView donde se mostrarán los datos y se manda a llamar la función "Execute" para realizar la petición de los datos

--- app/src/main/java/mx/leonardorivasd/examen_axity/MainActivity.java
+++ app/src/main/java/mx/leonardorivasd/examen_axity/MainActivity.java
@@ -2,6 +2,7 @@ package mx.leonardorivasd.examen_axity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvProducto = (TextView) findViewById(R.id.idProducto);
+        TextView tvDepartamento = (TextView) findViewById(R.id.idDepartamento);
+        TextView tvId = (TextView) findViewById(R.id.idCodigo);
+        TextView tvPrecio = (TextView) findViewById(R.id.idPrecio);
+
+        new GetData(tvProducto,tvDepartamento, tvId, tvPrecio, MainActivity.this).execute();
     }
 }
